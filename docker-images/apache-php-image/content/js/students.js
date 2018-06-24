e3c58154e47bdf2134b19e1bca3de3dd78c25bb8
simon <simon.jobin@heig-vd.ch>
2018-06-24 09:46:17 +0200

    add content to static image

---
+++ docker-images/apache-php-image/content/js/students.js
@@ -0,0 +1,17 @@
+$(function() {
+        console.log("Loading students");
+
+        function loadStudents(){
+                $.getJSON( "/api/students/", function( students){
+                        console.log(students);
+                        var message = "Nobody is here";
+                        if( students.length > 0){
+                                message = students[0].firstName + " " + students[0].lastName;
+                        }
+                        $(".container").text(message);
+                });
+        };
+
+        loadStudents();
+        setInterval(loadStudents, 2000);
+});
